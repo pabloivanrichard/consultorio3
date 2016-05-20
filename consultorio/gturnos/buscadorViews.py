8c0040f95e3261236167121e0828252aa3093d2e
Walter <wascasla@gmail.com>
2016-05-19 22:11:42 -0300

    Agrego el buscador de Pacientes, modifique el buscadorViews.py, forms.py y el template buscadorPacientes.html

--- consultorio/gturnos/buscadorViews.py
+++ consultorio/gturnos/buscadorViews.py
@@ -19,19 +19,64 @@ from .forms import *
 # 		form = buscadorForm()
 # 		return render(request, 'gturnos/selectores/buscadorPacientes.html', {'form':form})
 
+
+#------------------------------------------------------------------------------
+#esta funcion filtra los pacientes ya sea por documento, nombre y apellido
+#se pueden convinar
 def pacientesPorDni(request):
-	if request.method == "POST":		
-	    q = request.GET['dni']
-	    posts = Paciente.objects.filter(documento__search=q) #| \
-	                 #BlogPost.objects.filter(intro__search=q) | \
-	                 #BlogPost.objects.filter(content__search=q)
-	    return render_to_response(request, 'gturnos/selectores/buscadorPacientes.html', {'pacientes':pacientes})			
+	#pregunto si el metodo de envio de datos es POST(si se envian datos, si no devuelvo un formulario de busqueda vacio)
+	if request.method == "POST":	
+	#obtengo los valos de los inputTExt del formulario	
+	    q = request.POST['dni']
+	    q1 = request.POST['nombre']
+	    q2 = request.POST['apellido']
+	    #creo una lista vacia en caso de no encontrar ningun objeto
+	    pacientes = []
+
+	    #convino los filtros de busqueda teniendo en cuenta los campos que lleno el usuario en el fomrulario
+	    if q:
+	    	pacientes = Paciente.objects.filter(dni=q) 	#| \
+	    	#		Paciente.objects.filter(nombres__icontains=q1) | \
+	    	#		Paciente.objects.filter(apellido__icontains=q2)
+	    if q1:
+	    	pacientes = Paciente.objects.filter(nombres__icontains=q1)
+	    			
+	    if q2:
+	    	pacientes = Paciente.objects.filter(apellido__icontains=q2)	    
+	    if q and q1:
+	    	pacientes = Paciente.objects.filter(dni=q)# 	| \
+	    	pacientes = pacientes.filter(nombres__icontains=q1) #| \
+	    			#Paciente.objects.filter(apellido__icontains=q2)			
+	    if q and q2:
+	    	#pacientes = Paciente.objects.filter(dni=q) | \
+	    	#			Paciente.objects.filter(apellido__icontains=q2)
+	    	pacientes = Paciente.objects.filter(dni=q)
+	    	pacientes = pacientes.filter(apellido__icontains=q2)
+	    if q1 and q2:
+	    	pacientes = Paciente.objects.filter(nombres__icontains=q1)# | \
+	    	pacientes = pacientes.filter(apellido__icontains=q2)
+	    				#Paciente.objects.filter(apellido__icontains=q2)
+	    if q and q1 and q2:
+	    	pacientes = Paciente.objects.filter(dni=q)# 	| \
+	    	pacientes = pacientes.filter(nombres__icontains=q1)# | \
+	    	pacientes = pacientes.filter(apellido__icontains=q2)
+	    
+
+	    #creo un nuevo formulario
+	    form = buscadorForm()
+	    #pregunto si hubo resultado en la busqueda, si no devulvo el formulario para realizar otra consulta
+	    if len(pacientes) != 0:
+	    	
+	    	return render(request, 'gturnos/selectores/buscadorPacientes.html', {'pacientes':pacientes, 'form':form})			
+	    else:
+	    	return render(request, 'gturnos/selectores/buscadorPacientes.html', {'form':form})
+
 	else:		
 		form = buscadorForm()
 		return render(request, 'gturnos/selectores/buscadorPacientes.html', {'form':form})
 
 		#http://pythoncentral.io/how-to-use-python-django-forms/
-
+#------------------------------------------------------------------------------
 
 # def search(request):
 #     try:
